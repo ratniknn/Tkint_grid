8ede2169da86248ac325e246f2b013ac0f5dece6
ratniknn <71542865+ratniknn@users.noreply.github.com>
2023-05-31 22:11:05 +0300

    main_suffix.py в принципе работает, надо разблочить удаление Скорректировал описание программы Изменил типотбора файла по дате последнего изменения в файле, а не по дате создания

--- func_del_file.py
+++ func_del_file.py
@@ -3,7 +3,7 @@ import os
 import pathlib
 from pathlib import Path
 
-# path = r'D:\1\5429'
+# path = r'D:\1'
 # target_path = 'Doc_monitoring'
 # time_sec = 1687008500
 # file_suffix = ['.xlsx', '.JPG']
@@ -11,6 +11,7 @@ from pathlib import Path
 
 def del_file_suffix(path, target_path, time_sec, file_suffix):
     count = 0
+    print(path)
     # проходим методом glob() и walk() по выбранной директории и проверяем наличии пути до файла
     # проверка названия целевой папки. Если его нет подставляем **
     if target_path == '[Выбрать все файлы во всех папках]':
@@ -18,19 +19,25 @@ def del_file_suffix(path, target_path, time_sec, file_suffix):
     else:
         target_path_tmp = target_path + r'\**'
         print(target_path_tmp)
-    for resours_dirs in Path(path).glob('*'):
-        path_tmp = os.path.join(resours_dirs, target_path_tmp)
-        for i in glob.glob(path_tmp, recursive=True):
-            # print(i)
-            if pathlib.Path(i).suffix in file_suffix:
-                print(i)
-                if os.path.isfile(i):
-                    print(os.path.getctime(i))
-            if os.path.isfile(i) and pathlib.Path(i).suffix in file_suffix \
-                    and os.path.getctime(i) < time_sec:
-                # os.remove(i)
-                count += 1
-        return count
+    for j in os.listdir(path):
+        path_j = os.path.join(path, j)
+        if os.path.isdir(path_j):
+            print(j)
+            for resours_dirs in Path(path_j).glob('*'):
+                print(resours_dirs)
+                if target_path_tmp == '**' or resours_dirs.name in target_path:
+                    path_tmp = os.path.join(path_j, target_path_tmp)
+                    for i in glob.glob(path_tmp, recursive=True):
+                        print(i) # тестовый принт
+                        if pathlib.Path(i).suffix in file_suffix:# тестовый принт
+                            print(i)# тестовый принт
+                            if os.path.isfile(i):# тестовый принт
+                                print(os.path.getmtime(i))# тестовый принт
+                        if os.path.isfile(i) and pathlib.Path(i).suffix in file_suffix \
+                                and os.path.getmtime(i) < time_sec:
+                            # os.remove(i)
+                            count += 1
+    return count
 
 
 # print(del_file_suffix(path, target_path, time_sec, file_suffix))
--- main_suffix.py
+++ main_suffix.py
@@ -41,30 +41,34 @@ class Example(Frame):
 
         self.lbl = Label(self, text="Удаление файлов по дате")
         self.lbl.grid(row=0, sticky=W, pady=4, padx=5)
-        self.lbl1 = Label(self, text="1.Выберите папку с перечнем магазинов")
+        self.lbl1 = Label(self, text="1.Выберите папку с перечнем магазинов \n"
+                                     "2.Выберите общую папку из перечня, \nв которой надо удалить файлы \n"
+                                     "3.Выберите выберите дату, \nранее которой надо удалить файлы(отбор фалов ведется по дате \n последнего изменения/модернизации) \n"
+                                     "4.Выберите расширение для удаляемых файлов \n"
+                                     "5.Нажмите удалить файлы и подтвердите удаление. \nВНИМАНИЕ!! Файлы удаляются безвозвратно.")
         self.lbl1.grid(row=1, sticky=W, pady=4, padx=5)
-        self.lbl2 = Label(self, text="2.Выберите общую папку из перечня, \nв которой надо удалить файлы")
-        self.lbl2.grid(row=2, sticky=W, pady=4, padx=5)
-        self.lbl3 = Label(self, text="3.Выберите выберите дату, \nранее которой надо удалить файлы")
-        self.lbl3.grid(row=3, sticky=W, pady=4, padx=5)
-        self.lbl3 = Label(self, text="4.Нажмите удалить файлы и подтвердите удаление. \nВНИМАНИЕ!! Файлы удаляются безвозвратно.")
-        self.lbl3.grid(row=4, sticky=W, pady=4, padx=5)
+        # self.lbl2 = Label(self, text="2.Выберите общую папку из перечня, \nв которой надо удалить файлы")
+        # self.lbl2.grid(row=2, sticky=W, pady=4, padx=5)
+        # self.lbl3 = Label(self, text="3.Выберите выберите дату, \nранее которой надо удалить файлы")
+        # self.lbl3.grid(row=3, sticky=W, pady=4, padx=5)
+        # self.lbl3 = Label(self, text="4.Нажмите удалить файлы и подтвердите удаление. \nВНИМАНИЕ!! Файлы удаляются безвозвратно.")
+        # self.lbl3.grid(row=4, sticky=W, pady=4, padx=5)
 
         # строка для показа выбранного пути до основной папки
         self.path_del = Entry(self, width=56)
-        self.path_del.grid(row=5, column=0, columnspan=2, padx=5, sticky=W)
+        self.path_del.grid(row=2, column=0, columnspan=2, padx=5, sticky=W)
         # кнопка вызова окна выбора папки
         self.abtn = Button(self, text="Путь", command=self.OnOpen)
-        self.abtn.grid(row=5, column=3)
+        self.abtn.grid(row=2, column=3)
         # комбо-бокс для выбора папки внутри основной папки
         self.path_general = Combobox(self)
-        self.path_general.grid(row=6, column=0, columnspan=2, padx=5, sticky=W+E)
+        self.path_general.grid(row=3, column=0, columnspan=2, padx=5, sticky=W+E)
 
         # комбо-бокс для выбора папки внутри основной папки
         list_suffix = [['.xls', '.xlsx'], ['.doc', '.docx'], ['.jpeg', '.jpg', '.JPG'],
                        ['.dwg', '.DWG', '.bak', '.BAK'], ['.bak', '.BAK'] ]
         self.file_suffix_del = Combobox(self, values=list_suffix)
-        self.file_suffix_del.grid(row=7, column=0, columnspan=2, padx=5, sticky=W + E)
+        self.file_suffix_del.grid(row=4, column=0, columnspan=2, padx=5, sticky=W + E)
 
         # self.lbl_frame = Frame(self, width=55)
         # self.lbl_frame.grid(row=7, column=0, padx=5)
@@ -78,21 +82,22 @@ class Example(Frame):
         # создание поля выбора даты
         self.cal_del = DateEntry(self, width=12, hight=28, background='#3A81EA', locale='ru',
                                  foreground='white', borderwidth=5, border_radius=5, year=2016)
-        self.cal_del.grid(row=8, column=0, columnspan=2, padx=5, sticky="we")
-
-        self.btn_suf_del = Button(self, text='Удалить по \nрасширению')
-        self.btn_suf_del.grid(row=10, column=3)
+        self.cal_del.grid(row=5, column=0, columnspan=2, padx=5, sticky="we")
 
         self.cbtn = Button(self, text="Анализ", command=self.del_file_suf)
-        self.cbtn.grid(row=8, column=3, pady=4)
+        self.cbtn.grid(row=5, column=3, pady=4)
 
         self.txt_log = Text(self, width=40, height=8)
-        self.txt_log.grid(row=9, column=0, padx=5)
+        self.txt_log.grid(row=6, column=0, padx=5)
+
+        self.btn_suf_del = Button(self, text='Удалить по \nрасширению')
+        self.btn_suf_del.grid(row=7, column=0)
 
         self.enable =IntVar()
         self.no_del_path = Checkbutton(self, text='Удалять\nпустые \nпапки', variable=self.enable)
-        self.no_del_path.grid(row=9, column=3)
-        self.no_del_path.select()
+        self.no_del_path.grid(row=6, column=3)
+        tmp = self.enable.get()
+        print(tmp)
 
         # self.btn_del = Button(self, text="Удалить выбранное", command=self.del_file)
         # self.btn_del.grid(row=10, column=0, padx=5)
@@ -133,20 +138,19 @@ class Example(Frame):
         target_path=self.path_general.get()
         dt = self.cal_del.get_date()
         # перевод формата даты Datetime.Date в Строку(формат str)
-
         second = dt.ctime()
         sec_ctime = time.strptime(second)
         print(time.mktime(sec_ctime))
         # Перевод даты из строки в формат Datetime.Datetime (можно обрабатывать теперь спокойно)
         time_sec = time.mktime(sec_ctime)
-        date_maby = datetime.strptime(second, "%a %b %d %H:%M:%S %Y")
+        # date_maby = datetime.strptime(second, "%a %b %d %H:%M:%S %Y")
         file_suffix = self.file_suffix_del.get()
         result = askyesno(title="Подтвержение операции", message="Подтвердить операцию?")
         if result:
             showinfo("Результат", "Операция подтверждена")
             count_file = del_file_suffix(path, target_path, time_sec, file_suffix)
             date_file = dt.strftime('%d.%m.%Y')
-            text_f = '\nУдалено файлов ранее ' + date_file + ' - ' + str(count_file)
+            text_f = 'Удалено файлов ранее ' + date_file + ' - ' + str(count_file) + '\n'
             self.txt_log.insert('5.0', text_f)
         else:
             showinfo("Результат", "Операция отменена")
